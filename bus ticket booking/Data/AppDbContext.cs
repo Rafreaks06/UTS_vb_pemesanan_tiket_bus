870a5a56cf0906af6816273e7e21a64840c39bd6
diitzy <adityaproduction011@gmail.com>
2025-11-01 15:18:09 +0700

    fix indentation AppDbContext.cs

--- bus ticket booking/Data/AppDbContext.cs
+++ bus ticket booking/Data/AppDbContext.cs
@@ -1,8 +1,7 @@
 ﻿using bus_ticket_booking.Models;
 using Microsoft.EntityFrameworkCore;
-namespace bus_ticket_booking.Data
 
-{
+namespace bus_ticket_booking.Data {
     public class AppDbContext : DbContext
     {
         public DbSet<Passenger> Passengers { get; set; }
@@ -10,10 +9,7 @@ namespace bus_ticket_booking.Data
         public DbSet<Sale> Sales { get; set; }
 
         // Constructor untuk runtime (pakai dependency injection)
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         // ✅ Constructor tambahan untuk design-time (agar Add-Migration bisa jalan)
         public AppDbContext() { }
@@ -22,17 +18,14 @@ namespace bus_ticket_booking.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                // buat anggota kelompok inget ganti sesuai konfigurasi database masing-masing
+                // ⚠️ Ganti sesuai konfigurasi database masing-masing
                 optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=db_vb2_bus_ticketing;Username=postgres;Password=Admin");
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Passenger>()
-                .HasOne(p => p.Sale)
-                .WithOne(s => s.Passenger)
-                .HasForeignKey<Sale>(s => s.PassengerId);
+            modelBuilder.Entity<Passenger>().HasOne(p => p.Sale).WithOne(s => s.Passenger).HasForeignKey<Sale>(s => s.PassengerId);
 
             base.OnModelCreating(modelBuilder);
         }
